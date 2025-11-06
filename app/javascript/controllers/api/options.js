19100debc6452eb91be2e0d1fbfceb4dbf866b96
Isaias Piña <iax7@users.noreply.github.com>
2025-11-06 08:01:25 -0600

    step 2

--- app/javascript/controllers/api/options.js
+++ app/javascript/controllers/api/options.js
@@ -1,7 +1,7 @@
-function cr(method, subUrl, handler, text, color) {
+function cr(method, subUrl, text, color) {
   const url = `/api/v1/${subUrl}`;
   const getUrl = (id) => `${url}/${id}`;
-  return { method, url, handler, text, color, getUrl };
+  return { method, url, text, color, getUrl };
 }
 
 const colors = {
@@ -13,14 +13,14 @@ const colors = {
 }
 
 export const API_OPTIONS = {
-  Asst: cr('PUT', 'assist', 'renderAssistance', 'Asistencia', colors.default),
-  F_V3: cr('PUT', 'food/v/3', 'renderFood', 'Cena Viernes', colors.friday),
-  F_S1: cr('PUT', 'food/s/1', 'renderFood', 'Desayuno Sábado', colors.saturday),
-  F_S2: cr('PUT', 'food/s/2', 'renderFood', 'Comida Sábado', colors.saturday),
-  F_S3: cr('PUT', 'food/s/3', 'renderFood', 'Cena Sábado', colors.saturday),
-  F_D1: cr('PUT', 'food/d/1', 'renderFood', 'Desayuno Domingo', colors.sunday),
-  F_D2: cr('PUT', 'food/d/2', 'renderFood', 'Comida Domingo', colors.sunday),
-  F_D3: cr('PUT', 'food/d/3', 'renderFood', 'Cena Domingo', colors.sunday),
-  F_L1: cr('PUT', 'food/l/1', 'renderFood', 'Desayuno Lunes', colors.monday),
-  Ping: cr('GET', 'ping', 'renderPing', 'Ping', colors.default),
+  Asst: cr('PUT', 'assist', 'Asistencia', colors.default),
+  F_V3: cr('PUT', 'food/v/3', 'Cena Viernes', colors.friday),
+  F_S1: cr('PUT', 'food/s/1', 'Desayuno Sábado', colors.saturday),
+  F_S2: cr('PUT', 'food/s/2', 'Comida Sábado', colors.saturday),
+  F_S3: cr('PUT', 'food/s/3', 'Cena Sábado', colors.saturday),
+  F_D1: cr('PUT', 'food/d/1', 'Desayuno Domingo', colors.sunday),
+  F_D2: cr('PUT', 'food/d/2', 'Comida Domingo', colors.sunday),
+  F_D3: cr('PUT', 'food/d/3', 'Cena Domingo', colors.sunday),
+  F_L1: cr('PUT', 'food/l/1', 'Desayuno Lunes', colors.monday),
+  Ping: cr('GET', 'ping', 'Ping', colors.default),
 }
--- app/javascript/controllers/api_controller.js
+++ app/javascript/controllers/api_controller.js
@@ -2,8 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 import { API_OPTIONS } from "./api/options.js"
 
 export default class extends Controller {
-  static targets = ["id",
-    "banner", "bannerTitle", "bannerSpinner"
+  static targets = [
+    "id", "lastId", // navBar
+    "banner", "bannerTitle", "bannerSpinner", // Banner
+    "error", "debug", // Main content
   ]
 
   static values = {
@@ -28,8 +30,12 @@ export default class extends Controller {
 
   submit = async () => {
     const optionUrl = `${this.apiOptionsValue.url}/${this.idTarget.value}`;
+    this.lastIdTarget.innerHTML = `Último Id: <strong>${this.idTarget.value}</strong>`
+    this.showSpinner(true)
+    debugger
 
     try {
+      console.log(`Sending ${this.apiOptionsValue.method} request to ${optionUrl}`)
       const response = await fetch(optionUrl, {
         method: this.apiOptionsValue.method,
         headers: {
@@ -39,9 +45,8 @@ export default class extends Controller {
         }
       })
       const data = await response.json()
-      debugger
 
-
+      console.log('API Response:', data)
       if (!response.ok) {
         // Handle HTTP errors
         let errorMsg
@@ -60,27 +65,44 @@ export default class extends Controller {
           errorMsg = result.status || result.error || JSON.stringify(result)
         }
 
-        this.setLastId(id)
+
         this.showError(errorMsg)
       } else {
         // Success
-        this.errorTarget.style.display = 'none'
-        this.mainTarget.style.display = 'block'
-
-        // Call the appropriate handler
-        apiAction.handler(result)
+        this.showError(null)
       }
 
     } catch (error) {
       // Network or other errors
-      this.setLastId(id)
+      debugger
       this.showError(`Error de conexión: ${error.message}`)
     } finally {
       // Always execute cleanup
-      this.setLastId(id)
-      this.idTarget.value = ''
-      this.hideSpinner()
+      this.showSpinner(false)
       this.idTarget.focus()
     }
+  }
+
+  showSpinner(show) {
+    this.bannerSpinnerTarget.style.display = show ? 'inline-block' : 'none'
+  }
+
+  showError(message) {
+    if (!message || message.trim() === '') {
+      this.errorTarget.style.display = 'none'
+      return
+    }
+
+    this.errorTarget.style.display = 'block'
+    this.errorTarget.innerHTML = `
+    <h2>
+      <i class="fa-solid fa-circle-exclamation" aria-hidden="true"></i> Error<br><br>
+      <span>${message}</span>
+    </h2>
+    <code class="font-monospace"></code>`
+  }
+
+  showDebug(code) {
+    this.debugTarget.textContent = code
   }
 }
